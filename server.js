cce21873e4a7ecb8c3e7af611a6792b018d79bf3
Stephen Grice <stephengrice@live.com>
2016-12-21 13:35:57 -0500

    Change code to use Express and Nodemon

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-// Import HTTP Module
-var http = require('http');
+var express = require('express');
+var app = express();
+var port = 8080;
 
-// Handle sending requests, recv responses
-function handleRequests(req, res) {
-	// Return string (handle basic response)
-	res.end('Hello world!');
-}
-
-// Create the server
-var server = http.createServer(handleRequests);
-
-// Start server, listen on port
-server.listen(8080, function() {
-	console.log('Listening on port 8080');
+// Start the server
+app.listen(port, function() {
+	console.log('App started');
 });
+
+// Add a route
+// req is request, res is response
+app.get('/', function(req, res) { 
+	res.send('Hello world!');
+});
+
+// Package nodemon:
+	// Watch app for file changes
+	// Automatically restart it
+	// `npm install nodemon`
+	// instead of `node server.js`
+	// Run `nodemon server.js`
